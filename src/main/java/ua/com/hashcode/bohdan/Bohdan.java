cecc1c0cdcf6069f5691fd0a324ce54ac869a375
bohdan <bshevyrov@gmail.com>
2022-02-24 20:59:46 +0200

    start sort

--- src/main/java/ua/com/hashcode/bohdan/Bohdan.java
+++ src/main/java/ua/com/hashcode/bohdan/Bohdan.java
@@ -1,4 +1,14 @@
 package ua.com.hashcode.bohdan;
 
+import ua.com.hashcode.sancho.Project;
+
+import java.util.List;
+
 public class Bohdan {
+    private static List<Project> sortProject (List<Project> listProject) {
+
+
+    }
+
+
 }
